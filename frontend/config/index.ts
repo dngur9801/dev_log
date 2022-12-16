1b4ccf8b9438222b9fda8a9c3bbd15585f82b194
dngur9801 <dngur9801@gmail.com>
2022-12-16 20:53:43 +0900

    config 폴더 추가

---
+++ frontend/config/index.ts
@@ -0,0 +1,36 @@
+export function apiAddress() {
+  return process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.NEXT_PUBLIC_API_ADDRESS;
+}
+
+export function defaultProfileImage() {
+  return '/image/profile.png';
+}
+
+export function defaultTitleImage() {
+  return '/image/noimg.jpeg';
+}
+
+export const seoConfig = {
+  titleTemplate: '%s | Devlog',
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'manifest',
+      href: '/manifest.json',
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: 'theme-color',
+      content: '#FFFFFF',
+    },
+  ],
+  openGraph: {
+    type: 'website',
+    site_name: 'Devlog',
+    images: [{ url: 'https://example.com/example_square_image.png' }],
+  },
+};
