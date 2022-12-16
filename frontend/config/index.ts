da67ff867358d5bff0c1c7b4bf8f2a1daad96717
dngur9801 <dngur9801@gmail.com>
2022-12-17 01:17:20 +0900

    api 주소 변경

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,5 +1,6 @@
 export function apiAddress() {
-  return process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.NEXT_PUBLIC_API_ADDRESS;
+  return process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'http://localhost:5000';
+  // : process.env.NEXT_PUBLIC_API_ADDRESS;
 }
 
 export function defaultProfileImage() {
