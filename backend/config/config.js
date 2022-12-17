f0488765d96be5bb04f445d04c089e3a464118eb
dngur9801 <dngur9801@gmail.com>
2022-12-18 08:51:47 +0900

    modify

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log('process.env.API_ADDRESS : ', process.env.API_ADDRESS);
 module.exports = {
   development: {
     username: 'root',
     password: process.env.DB_PASSWORD,
     database: 'devlog',
     // host: '127.0.0.1',
-    host: process.env.API_ADDRESS,
     dialect: 'mysql',
   },
   production: {
