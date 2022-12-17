3ab3535c77d476e4550362d43fcb578a7925e464
dngur9801 <dngur9801@gmail.com>
2022-12-18 08:08:19 +0900

    modify

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,8 @@
 const dbConfig = require('../config/config')['development'];
 
+console.log('dbConfig.database : ', dbConfig.database);
+console.log('dbConfig.username : ', dbConfig.username);
+console.log('dbConfig.password : ', dbConfig.password);
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   dbConfig.database,
