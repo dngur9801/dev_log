4488e24cd7daefea93c1b2d10de35402064fa46f
dngur9801 <dngur9801@gmail.com>
2022-12-20 23:48:33 +0900

    deploy test

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,12 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  cookieParser(process.env.COOKIE_SECRET, {
+    sameSite: true,
+    secure: true,
+  })
+);
 app.use(
   cors({
     origin:
@@ -54,7 +59,7 @@ app.use(
       httpOnly: true,
       sameSite: 'None',
       // secure: true,
-      domain: '.app',
+      // domain: '.app',
       // process.env.NODE_ENV === 'production' && process.env.CLIENT_ADDRESS,
     },
   })
