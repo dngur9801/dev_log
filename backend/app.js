b99ada3c35d20ebaae1add1b7fbdaca9714ffcf8
dngur9801 <dngur9801@gmail.com>
2022-12-20 23:48:51 +0900

    deploy test

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cookieParser(process.env.COOKIE_SECRET, {
-    sameSite: true,
+    sameSite: 'None',
     secure: true,
   })
 );
