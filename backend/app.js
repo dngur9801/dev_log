6ad624a440c4255c9a9d723ed24bcbb33740dac1
dngur9801 <dngur9801@gmail.com>
2022-12-24 02:30:12 +0900

    로그인 테스트2

--- backend/app.js
+++ backend/app.js
@@ -51,8 +51,7 @@ app.use(
       httpOnly: true,
       sameSite: process.env.NODE_ENV === 'production' && 'None',
       secure: process.env.NODE_ENV === 'production',
-      domain:
-        process.env.NODE_ENV === 'production' && 'devlog-murex.vercel.app',
+      domain: process.env.NODE_ENV === 'production' && process.env.API_ADDRESS,
       maxAge: new Date(Date.now() + 1000 * 60 * 60 * 24),
     },
   })
