098f199bb21a546f9d1cd8e47e7d452e819a48a7
dngur9801 <dngur98012@naver.com>
2023-01-14 17:24:23 +0900

    origin 주소 변경

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   cors({
-    origin: location + '/api',
+    origin: location,
     credentials: true,
     allowedHeaders: 'Content-Type',
   })
