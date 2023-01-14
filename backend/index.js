a84eec48e51acac171081faf526813d6c9a932d2
dngur9801 <dngur98012@naver.com>
2023-01-14 17:36:59 +0900

    maxage 삭제

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,6 @@ app.use(
       sameSite: process.env.NODE_ENV === 'production' && 'None',
       secure: process.env.NODE_ENV === 'production',
       domain: process.env.NODE_ENV === 'production' && '.devlog.shop',
-      maxAge: new Date(Date.now() + 1000 * 60 * 60 * 24),
     },
   })
 );
